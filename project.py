12a52645b834fa0f30ad42a6889a679a80a24d8f
riri7282 <rxt7282@nyu.edu>
2025-04-23 21:04:23 -0400

    Part 3 Aug

--- project.py
+++ project.py
@@ -27,3 +27,43 @@ def load_data(data_dir='/content/drive/MyDrive/dl4m/dl4m-group7-main/data', batc
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader, dataset.classes
+
+def load_data_with_augmentation(data_dir='/content/drive/MyDrive/dl4m/dl4m-group7-main/data', batch_size=32, image_size=(224, 224)):
+    """
+    Loads the dataset with data augmentation applied only to the training split.
+    Test set is loaded with standard transforms (no augmentation).
+    """
+    # Augmentation for training set
+    train_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.Resize(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+    # Standard transform for test set
+    test_transform = transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+    # Load full dataset with training transform (we’ll change test's transform manually later)
+    dataset = datasets.ImageFolder(root=data_dir, transform=train_transform)
+
+    # Split into train/test (80/20)
+    total_size = len(dataset)
+    train_size = int(0.8 * total_size)
+    test_size = total_size - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+    # Manually set transform for test set to non-augmented
+    test_dataset.dataset.transform = test_transform
+
+    # Create DataLoaders
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader, dataset.classes
+
